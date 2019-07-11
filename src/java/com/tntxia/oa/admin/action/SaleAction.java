91a939b7098f1e31dac6933c445750b1d09a8384
tntxia <tntxia@163.com>
2019-07-12 03:00:07 +0800

    销售单支持后台删除

--- src/java/com/tntxia/oa/admin/action/SaleAction.java
+++ src/java/com/tntxia/oa/admin/action/SaleAction.java
@@ -21,15 +21,22 @@ public class SaleAction extends BaseAction {
 	@SuppressWarnings("rawtypes")
 	public Map<String, Object> list(WebRuntime runtime) throws Exception{
 		
+		String sqlWhere = " where 1 = 1";
+		
+		String number = runtime.getParam("number");
+		if (StringUtils.isNotEmpty(number)) {
+			sqlWhere += " and number like '%" + number + "%'";
+		}
+		
 		PageBean pageBean = runtime.getPageBean();
-		String sql = "select top "+pageBean.getTop()+" * from subscribe where 1=1";
+		String sql = "select top "+pageBean.getTop()+" * from subscribe"+ sqlWhere;
 		List list = dbManager.queryForList(sql, true);
-		int count = dbManager.getCount("select count(*) from subscribe where 1=1");
+		int count = dbManager.getCount("select count(*) from subscribe " + sqlWhere);
 		return this.getPagingResult(list, pageBean, count);
 		
 	}
 	
-	private void delOrder(Integer id ) throws SQLException {
+	private void delOrder(String id ) throws SQLException {
 		String sql = "delete from subscribe where id = ?";
 		String sqlPro = "delete from ddpro where ddid = ?";
 		Transaction trans = this.getTransaction("oa");
@@ -46,37 +53,11 @@ public class SaleAction extends BaseAction {
 		
 	}
 	
-	@SuppressWarnings("rawtypes") 
 	public Map<String,Object> batchDel(WebRuntime runtime) throws Exception{
-		String sdate = runtime.getParam("sdate");
-		String edate = runtime.getParam("edate");
+		String ids = runtime.getParam("ids");
 		
-		
-		List param = new ArrayList();
-		String sqlWhere = "";
-		
-		if(StringUtils.isNotEmpty(sdate)) {
-			
-			sqlWhere += " and datetime >= ? ";
-			param.add(sdate);
-		}
-		
-		if(StringUtils.isNotEmpty(edate)) {
-			String nextDate = DateUtil.getNextDate(edate); 
-			sqlWhere += " and datetime < ? ";
-			param.add(nextDate);
-		}
-		
-		String sql = "select * from subscribe where 1=1 " + sqlWhere;
-		List list = dbManager.queryForList(sql, param, true);
-		
-		System.out.println("查询到"+ list.size()+"条记录。。。 正在准备删除");
-		
-		for(int i=0;i<list.size();i++) {
-			Map map = (Map) list.get(i);
-			Integer id = (Integer) map.get("id");
+		for(String id : ids.split(",")) {
 			this.delOrder(id);
-			System.out.println("已删除"+i+"条记录");
 		}
 		
 		return this.success();
